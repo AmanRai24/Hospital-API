c7fe3bc2f9f2ccf18d898af21ec142ca4110f8f3
Souvik Khan <souvik97381@gmail.com>
2023-04-17 22:56:07 +0530

    third commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/HospitalAPI',{useNewUrlParser: true});
+const connection = "mongodb+srv://souvikmukherjee089:Souvik12345@hospital-api-cluster.5ppzfch.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(connection, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
-db.once('open', function(){
+db.once('open', function () {
     console.log('Connected to Database :: MongoDB');
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express=require('express'); 
+const express = require('express');
 const app = express();
-const port = 8000;
+const port = 9000;
 
 const db = require('./config/mongoose');
 
@@ -14,28 +14,28 @@ const morgan = require("morgan");
 //require passport and JWT Strategy for auth
 const passport = require('passport');
 //use of JWT token
-const passportJWT=require('./config/passport-jwt-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 
 //do not show the log for test
 if (config.util.getEnv("NODE_ENV") !== "test") {
     //use of morgan library for command line
     app.use(morgan("combined"));
-  }
+}
 
 
 //parse application/json and look for raw text  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type:"application/json" }));
+app.use(bodyParser.json({ type: "application/json" }));
 
 app.use(passport.initialize());
 //use express router
-app.use('/',require('./routes/index'));
+app.use('/', require('./routes/index'));
 
-//server running on port 8000
-app.listen(port, function(err){
-    if (err){
+//server running on port 9000
+app.listen(port, function (err) {
+    if (err) {
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running on port: ${port}`);
